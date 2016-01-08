b8d617f399d31e6e0b526cf386983158350741c1
redmechanic <redmechanic_@outlook.com>
2016-01-08 01:51:49 +0000

    Basic mine placing and number calculating done.

--- Minesweeper.NET/Minesweeper.NET/Form1.vb
+++ Minesweeper.NET/Minesweeper.NET/Form1.vb
@@ -7,13 +7,31 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         vbgame.setDisplay(Me, "1280x720")
+        thread.Start()
+
+        numbers.images.Add(-1, vbgame.getImage("-1.png"))
+        numbers.images.Add(1, vbgame.getImage("1.png"))
+        numbers.images.Add(2, vbgame.getImage("2.png"))
+        numbers.images.Add(3, vbgame.getImage("3.png"))
+        numbers.images.Add(4, vbgame.getImage("4.png"))
+        numbers.images.Add(5, vbgame.getImage("5.png"))
+        numbers.images.Add(6, vbgame.getImage("6.png"))
+        numbers.images.Add(7, vbgame.getImage("7.png"))
+        numbers.images.Add(8, vbgame.getImage("8.png"))
+
     End Sub
 
     Sub gameloop()
         Dim run As Boolean = True
+        Dim minegrid As New MineGrid(vbgame)
         While run
 
+            vbgame.fill(Color.FromArgb(150, 150, 150))
 
+            minegrid.handleCells()
+
+            vbgame.update()
+            vbgame.clockTick(60)
 
         End While
     End Sub
--- Minesweeper.NET/Minesweeper.NET/MineGrid.vb
+++ Minesweeper.NET/Minesweeper.NET/MineGrid.vb
@@ -1,35 +1,73 @@
-﻿Class Cell
+﻿Class numbers
+    Public Shared images As New Dictionary(Of Integer, System.Drawing.Image)
+End Class
+
+Class Cell
 
     Public vbgame As VBGame
     Public type As String = "blank"
+    Public flagged As Boolean = False
     Public number As Integer = 0
-    Public button As New Button
+    Public group As Integer
+    Public x, y, side As Integer
     Public dug As Boolean = False
-    Public colors As Dictionary(Of Integer, System.Drawing.Color)
+    Public cooldown As Integer = 0
+    'Public colors As New Dictionary(Of Integer, System.Drawing.Color)
 
-    Sub New(ByRef display As VBGame, Optional x As Integer = 0, Optional y As Integer = 0, Optional side As Integer = 10)
+    Sub New(ByRef display As VBGame, Optional xt As Integer = 0, Optional yt As Integer = 0, Optional sidet As Integer = 10)
 
-        colors.Add(0, Color.FromArgb(200, 200, 200))
-        colors.Add(1, Color.FromArgb(0, 0, 255))
-        colors.Add(2, Color.FromArgb(0, 128, 0))
-        colors.Add(3, Color.FromArgb(255, 0, 0))
-        colors.Add(4, Color.FromArgb(0, 0, 128))
-        colors.Add(5, Color.FromArgb(128, 0, 0))
-        colors.Add(6, Color.FromArgb(0, 128, 128))
-        colors.Add(7, Color.FromArgb(0, 0, 0))
-        colors.Add(8, Color.FromArgb(128, 128, 128))
+        'colors.Add(0, Color.FromArgb(200, 200, 200))
+        'colors.Add(1, Color.FromArgb(0, 0, 255))
+        'colors.Add(2, Color.FromArgb(0, 128, 0))
+        'colors.Add(3, Color.FromArgb(255, 0, 0))
+        'colors.Add(4, Color.FromArgb(0, 0, 128))
+        'colors.Add(5, Color.FromArgb(128, 0, 0))
+        'colors.Add(6, Color.FromArgb(0, 128, 128))
+        'colors.Add(7, Color.FromArgb(0, 0, 0))
+        'colors.Add(8, Color.FromArgb(128, 128, 128))
 
         vbgame = display
-        button.useDisplay(vbgame)
-        button.setRect(New Rectangle(x, y, side, side))
-        button.setColor(Color.FromArgb(200, 200, 200), Color.FromArgb(128, 128, 128))
+
+        x = xt
+        y = yt
+        side = sidet
     End Sub
+
+    Function getRect()
+        Return New Rectangle(x, y, side, side)
+    End Function
 
     Function handle() As String
         Dim clicked As Boolean = False
-        vbgame.drawCenteredText(button.getRect(), CStr(number), colors(number))
         If Not dug Then
-            clicked = button.handle()
+            clicked = False 'button.handle()
+
+            vbgame.drawRect(getRect(), Color.FromArgb(200, 200, 200))
+            vbgame.drawRect(New Rectangle(x + 2, y + 2, side - 4, side - 4), Color.FromArgb(250, 250, 250))
+            If Not IsNothing(vbgame.mouse) Then
+                If vbgame.collideRect(New Rectangle(vbgame.mouse.Location().X, vbgame.mouse.Location().Y, 1, 1), getRect()) Then
+                    'vbgame.drawRect(getRect(), Color.FromArgb(128, 128, 128))
+                    If vbgame.mouse.Button = vbgame.mouse_left And Not flagged Then
+                        clicked = True
+                    ElseIf vbgame.mouse.Button = vbgame.mouse_right Then
+                        If cooldown <= 0 Then
+                            cooldown = 10
+                            If flagged Then
+                                flagged = False
+                            Else
+                                flagged = True
+                            End If
+                        Else
+                            cooldown -= 1
+                        End If
+                    End If
+                End If
+            End If
+
+            If flagged Then
+                vbgame.drawRect(New Rectangle(x + 5, y + 5, side - 10, side - 10), vbgame.red)
+            End If
+
 
             If clicked Then
                 dug = True
@@ -41,8 +79,19 @@
                     Return "dug"
                 End If
             End If
-        End If
-        Return "nothing"
+        Else
+            If number <> 0 Then
+                vbgame.blit(numbers.images(number), getRect())
+            End If
+            If Not IsNothing(vbgame.mouse) Then
+                If vbgame.collideRect(New Rectangle(vbgame.mouse.Location().X, vbgame.mouse.Location().Y, 1, 1), getRect()) Then
+                    If vbgame.mouse.Button = vbgame.mouse_right Then
+                        Return "dig9"
+                    End If
+                End If
+            End If
+            End If
+            Return "nothing"
     End Function
 
 End Class
@@ -52,28 +101,134 @@ Public Class MineGrid
     Public vbgame As VBGame
     Public cells As Array
     Public gridsize As Integer
+    Public random As New Random
 
-    Sub New(ByRef display As VBGame, Optional gridsizet As Integer = 10)
-        Dim x, y As Integer
+    Sub New(ByRef display As VBGame, Optional gridsizet As Integer = 20, Optional mines As Integer = 300)
+        Dim x, y, n, i As Integer
         gridsize = gridsizet
         vbgame = display
-        cells = cells((vbgame.width - gridsize) / gridsize, (vbgame.height - gridsize) / gridsize)
+        Dim cellst((vbgame.width - gridsize) / gridsize, (vbgame.height - gridsize) / gridsize) As Cell
 
+        cells = cellst
         For x = 0 To (vbgame.width - gridsize) / gridsize
             For y = 0 To (vbgame.height - gridsize) / gridsize
 
-                cells(x, y) = New Cell(vbgame, x, y, gridsize)
+                cells(x, y) = New Cell(vbgame, x * gridsize, y * gridsize, gridsize)
+
+            Next
+        Next
+
+        For i = 1 To mines
+            x = random.Next(0, (vbgame.width - gridsize) / gridsize)
+            y = random.Next(0, (vbgame.height - gridsize) / gridsize)
+            cells(x, y).number = -1
+            cells(x, y).type = "mine"
+        Next
+
+        For x = 0 To (vbgame.width - gridsize) / gridsize
+            For y = 0 To (vbgame.height - gridsize) / gridsize
+                If cells(x, y).type = "mine" Then
+                    Continue For
+                End If
+                n = 0
+                Try
+                    If cells(x - 1, y - 1).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x + 1, y - 1).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x - 1, y + 1).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x + 1, y + 1).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x - 1, y).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x, y - 1).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x + 1, y).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+                Try
+                    If cells(x, y + 1).type = "mine" Then
+                        n += 1
+                    End If
+                Catch ex As Exception
+                End Try
+
+                cells(x, y).number = n
 
             Next
         Next
     End Sub
 
-    Sub draw()
-        Dim x, y As Integer
+    Sub handleCells()
+        Dim tx, ty, x, y As Integer
         Dim cmd As String
-        For x = 0 To (vbgame.width - gridsize) / gridsize
-            For y = 0 To (vbgame.height - gridsize) / gridsize
+        For tx = 0 To (vbgame.width - gridsize) / gridsize
+            For ty = 0 To (vbgame.height - gridsize) / gridsize
+                x = tx
+                y = ty
                 cmd = cells(x, y).handle()
+
+                If cmd = "dig9" Then
+                    Try
+                        cells(x - 1, y - 1).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x + 1, y - 1).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x - 1, y + 1).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x + 1, y + 1).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x - 1, y).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x, y - 1).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x + 1, y).dug = True
+                    Catch ex As Exception
+                    End Try
+                    Try
+                        cells(x, y + 1).dug = True
+                    Catch ex As Exception
+                    End Try
+                End If
             Next
         Next
     End Sub

bdbbd188047e71cdb5f483deb95eaec38ca79aba
redmechanic <redmechanic_@outlook.com>
2016-01-27 19:16:30 +0000

    Cleaning up and better mouse handling.

--- Minesweeper.NET/Minesweeper.NET/Form1.vb
+++ Minesweeper.NET/Minesweeper.NET/Form1.vb
@@ -5,16 +5,25 @@ Public Class Form1
     Public thread As New Thread(AddressOf gameloop)
     Public vbgame As New VBGame
 
+    Dim side As Integer = 20
+    Dim mines As Integer = 400
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        vbgame.setDisplay(Me, "1280x720")
+        Dim width, height As Integer
+        'width = InputBox("Grid width?")
+        'height = InputBox("Grid Height?")
+        'mines = InputBox("Mines?")
+        width = 30
+        height = 16
+        mines = 99
+        vbgame.setDisplay(Me, width * side & "x" & height * side, "Minesweeper.NET")
+        'vbgame.setDisplay(Me, "800x800", "Minesweeper.NET")
         thread.Start()
 
     End Sub
 
     Sub gameloop()
         Dim run As Boolean = True
-        Dim side As Integer = 20
-        Dim mines As Integer = 400
         vbgame.fill(Color.FromArgb(150, 150, 150))
         Dim minegrid As New MineGrid(vbgame, side, mines)
 
@@ -32,9 +41,14 @@ Public Class Form1
                 End If
             Next
 
-            vbgame.fill(Color.FromArgb(150, 150, 150))
+            For Each e In vbgame.getMouseEvents()
+                If e.action = MouseEvent.MouseDown Then
+                    Console.WriteLine(e.button)
+                    minegrid.handleCells(e)
+                End If
+            Next
 
-            minegrid.handleCells()
+            minegrid.drawCells()
 
             For Each effect In cross.crosses.ToList()
                 If effect.opacity = 0 Then
